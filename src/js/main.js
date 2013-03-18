f8ae57a60f2a5b9b845e9c6031a3d0043f2bdd71
unknown <student-pmi@PC3-106-1.vyatsu.ru>
2013-03-18 12:31:30 +0400

    end day commit

--- src/js/main.js
+++ src/js/main.js
@@ -10,9 +10,6 @@ var running = false;
 var unit = 1;
 var objectsSegments = 10;
 
-Physijs.scripts.worker = 'physijs_worker.js';
-Physijs.scripts.ammo = 'ammo.js';
-
 function init(container) {
 	if (Detector.webgl)
 		renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -43,8 +40,6 @@ function init(container) {
 	$(window).resize(function() {
 		resize();
 	});
-
-	
 };
 
 function start() {
@@ -80,7 +75,6 @@ function update() {
 		for (var i = 0; i < objects.length; i++) {
 			objects[i].update(delta);
 		}
-
 };
 
 function randomNumber(min, max) {
@@ -102,11 +96,13 @@ function addNanoObject(position, rotation, speed, radius, length) {
 		new THREE.CylinderGeometry(radius, radius, length, objectsSegments, false),
 		new THREE.MeshLambertMaterial({color: 0xff0000}));
 	object.geometry.computeBoundingBox();
+	object.radius = radius;
+	object.length = length;
 	var bbox = object.geometry.boundingBox;
 
 	object.add(new THREE.Mesh(
 		new THREE.CubeGeometry(bbox.max.x - bbox.min.x, bbox.max.y - bbox.min.y, bbox.max.z - bbox.min.z),
-		new THREE.MeshLambertMaterial({wireframe: true, wireframe_linewidth: 10, color: 0x00ff00})));
+		new THREE.MeshLambertMaterial({wireframe: true, wireframe_linewidth: 1, color: 0x00ff00})));
 
 	object.position = position;
 	object.rotation = rotation;
@@ -136,15 +132,75 @@ function randomNanoObjects(n, radius) {
 			unit,
 			10 * unit);
 	}
-}
+};
 
+function scaleObject(index) {
+
+};
+/*
+function Nanotube(object, n, m, d) {
+	var atoms = new Array(n);
+
+	object.
+
+	(function() {
+		var dy = d / 2;
+		var dx = Math.sqrt(d * d - dy * dy);
+		var width = 2 * m * dx;
+		var height = Math.floor((n - 1) / 2) * 1.5 * d + (n - 1) % 2 * d / 2;
+
+		self.width = width;
+		self.height = height;
+
+		var a = 2 * Math.PI / m;
+		var radius = d / (2 * Math.sin(a / 4));
+		self.radius = radius;
+		var y = 0;
+
+
+		for(var i = 0; i < n; i++) {
+			self.atoms[i] = new Array(m);
+			for(var j = 0; j < m; j++)
+				self.atoms[i][j] = new Nanocollider.Atom(new THREE.Vector3(0, 0, 0), unit / 4, self);
+		}
+
+		for(var i = 0; i < n; i++) {
+			var r = (i + 1) % 4 < 2 ? 0 : 1;
+			var x = 0;
+
+			for(var j = 0; j < m; j++) {
+
+				var alpha = j * a + r * Math.PI / m;
+				x = radius * Math.sin(alpha);
+				z = radius * Math.cos(alpha);
+
+				self.atoms[i][j].mesh.position = new THREE.Vector3(
+					self.position.x + x,
+					self.position.y + y - height / 2,
+					self.position.z + z
+					);
+				if(drawAtoms)
+					self.mesh.add(self.atoms[i][j].mesh);
+				if(i > 0) {
+					self.mesh.add((new Nanocollider.Connection(self.atoms[i - 1][j].mesh.position, self.atoms[i][j].mesh.position, colorConnection)).mesh);
+
+					if (i % 2 == 1 && r == 0)
+						self.mesh.add((new Nanocollider.Connection(self.atoms[i - 1][(j - 1 + m) % m].mesh.position, self.atoms[i][j].mesh.position, colorConnection)).mesh);
+					
+					if (i % 2 == 1 && r == 1)
+						self.mesh.add((new Nanocollider.Connection(self.atoms[i - 1][(j + 1 + m) % m].mesh.position, self.atoms[i][j].mesh.position, colorConnection)).mesh);
+				}
+			}
+			y += i % 2 == 0 ? dy : d;  
+		}
+	})();
+};
+*/
 $(document).ready(function() {
 	init(document.getElementById("canvas_container"));
 	start();
 
-
 	$('#button_test1').click(function() {
-		//addNanoObject(new THREE.Vector3(), unit, 10 * unit);
 		randomNanoObjects(100, 100 * unit);
 	});
 
